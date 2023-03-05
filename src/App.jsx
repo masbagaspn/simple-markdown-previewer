623ed2fb6216e192cf5c7c49cc3dbeabc392b174
masbagaspn <masbagaspn@gmail.com>
2023-03-05 17:53:52 +0700

    fix: remove white space after break line on initial markdown state

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { marked } from 'marked'
 import './App.css'
 
 function App() {
-  const [markdown, setMarkdown] = useState("# Here's and example on how to use the markdown \n ## Sub Heading \n [Links](https://www.freecodecamp.org) `Inline Code` **Bold Text** \n\n ```\nMulti-line code\n```\n\n > Block Quotes! \n\n Unordered List\n- List\n  - List\n\nOrdered List\n1. List\n1. List\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)")
+  const [markdown, setMarkdown] = useState("# Here's and example on how to use the markdown \n## Sub Heading \n[Links](https://www.freecodecamp.org) `Inline Code` **Bold Text** \n\n```\nMulti-line code\n```\n\n> Block Quotes!\n\nUnordered List\n- List\n  - List\n\nOrdered List\n1. List\n1. List\n\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)")
   
   const raw = marked(markdown, { breaks: true, gfm: true })
 
